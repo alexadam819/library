0c146dbd753bb444159e02ed2ff060c5ca4b0f51
Alexander Adam <alexadam@gmail.com>
2016-01-31 21:24:02 -0500

    Adding the project to github

---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,54 @@
+var express = require('express');
+
+var bookRouter = express.Router();
+
+var router = function(nav){
+    var books = [
+    {
+        title: 'The Wind in the Willows',
+        genre: 'Fantasy',
+        author: 'Kenneth Grahame',
+        read: false
+    },
+    {
+        title: 'Life On The Mississippi',
+        genre: 'History',
+        author: 'Mark Twain',
+        read: false
+    },
+    {
+        title: 'Les Miserable',
+        genre: 'Historial Fiction',
+        author: 'Victor Hugo',
+        read: false
+    },
+    {
+        title: 'The Time Machine',
+        genre: 'Science Fiction',
+        author: 'H. G. Wells',
+        read: false
+    },
+]; 
+bookRouter.route('/')
+    .get(function(req, res){
+        res.render('bookListView', {
+            title: 'Books',
+            nav: nav,
+            books: books 
+        });
+    });
+    
+bookRouter.route('/:id')
+    .get(function(req, res){
+        var id = req.params.id;
+        res.render('bookView', {
+            title: 'Books',
+            nav: nav,
+            books: books[id]
+        });
+    });
+    return bookRouter;
+};
+
+    
+module .exports = router;
